27f8653ab0d907da342e609f5da4941bb6022707
AlihanFemi <147508857+AlihanFemi@users.noreply.github.com>
2023-12-06 16:47:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueCharInString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var repeating = new List<char>();
+            string str = Console.ReadLine();
+            string unique = "";
+            foreach(char c in str)
+            {
+                if (unique.Contains(c))
+                    repeating.Add(c);
+                else
+                    unique += c;
+            }
+            if(unique.Length > 0)
+            {
+                Console.WriteLine("Duplicates found! Here are they: ");
+                Console.WriteLine(String.Join(", ", repeating));
+            }
+            else 
+                Console.WriteLine("All unique!");
+        }
+    }
+}
